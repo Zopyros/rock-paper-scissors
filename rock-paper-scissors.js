3e67eb847e5beb0660361c9939d919433321ea7f
Christian <cmanalo.io@outlook.com>
2022-09-21 02:44:14 -0700

    Create function comparing player1 and computer

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -4,8 +4,46 @@ let rock, paper, scissors
 
 function playRound(){
 
-const playerSelection = "rock"
+const playerSelection = prompt('Rock Paper or Scissors?').toLowerCase()
 const computerSelection = getComputerChoice()
+
+
+if(playerSelection === "rock" && computerSelection === "rock"){
+    alert('TIE Game!')
+
+}
+else if (playerSelection === "rock" && computerSelection === "paper"){
+    alert('You Lose. Paper beats Rock')
+}
+
+else if (playerSelection === "rock" && computerSelection === "scissors"){
+    alert('You Win! Rock beats Scissors.')
+}
+else if(playerSelection === "paper" && computerSelection === "rock"){
+    alert('You Win! Paper beats Rock')
+}
+else if (playerSelection === "paper" && computerSelection === "paper"){
+    alert('TIE Game!')
+}
+
+else if (playerSelection === "paper" && computerSelection === "scissors"){
+    alert('You Lose! Scissors beats Paper')
+}
+else if(playerSelection === "scissors" && computerSelection === "rock"){
+    alert('You Lose! Rock beats Scissors')
+}
+else if (playerSelection === "scissors" && computerSelection === "paper"){
+    alert('You Won! Scissors beats Paper.')
+}
+
+else if (playerSelection === "scissors" && computerSelection === "scissors"){
+    alert('TIE Game!')
+}
+
+else {
+    document.querySelector('h1').innerHTML = "ERROR"
+}
+
 
 }
 
@@ -27,7 +65,8 @@ function getComputerChoice(){
    
 }
 
-console.log(getComputerChoice())
+
+playRound()
 
 
 
